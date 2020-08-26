c77158b45e310af858420cf9ce509765ac339eaa
Akhil <mirya003@umn.edu>
2020-08-26 18:49:13 -0500

    commit

--- app.py
+++ app.py
@@ -6,20 +6,25 @@ db = SqliteDatabase('inventory.db')
 
 class Product(Model):
     product_id = AutoField(primary_key=True)
-    product_name = CharField(max_length = 255)
-    product_quantity = IntegerField()
-    product_price = IntegerField()
-    date_updated = DateTimeField()
+    product_name = TextField(default="")
+    product_quantity = IntegerField(default=0)
+    product_price = IntegerField(default=0)
+    date_updated = DateTimeField(default=0)
 
     class Meta:
         database = db
 
-if __name__ == "__main__":
+def initialize():
     db.connect()
     db.create_tables([Product],safe=True)
+
+if __name__ == "__main__":
+    initialize()
 
     with open('inventory.csv',newline='') as csvfile:
         product_reader = csv.reader(csvfile,delimiter=',')
         rows = list(product_reader)
         for row in rows:
             Product.create(content=row)
+
+print(db.select())
